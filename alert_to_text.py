9bf4b106c040a6560f6e5e5a1c021eb365400408
Slavetomints <161798204+Slavetomints@users.noreply.github.com>
2024-06-06 23:30:09 -0500

    got alerts.txt to work

--- alert_to_text.py
+++ alert_to_text.py
@@ -1,16 +1,13 @@
 from nws_api import fetch_alerts, parse_alerts
 
-
-
 def main():
-    xml_data = fetch_alerts()
-    if xml_data:
-        alerts = parse_alerts(xml_data)
-        with open('alerts.txt', 'a') as file:
-          for alert in alerts:
-            # Write the desired text to the file
-            file.write(alert + "\n")
-
+  xml_data = fetch_alerts()
+  if xml_data:
+    alerts = parse_alerts(xml_data)
+    if alerts:
+      with open('alerts.txt', 'w') as file:
+        for alert in alerts:
+          file.write(alert['title'] + '\n')
 
 if __name__ == "__main__":
-    main()
+  main()
